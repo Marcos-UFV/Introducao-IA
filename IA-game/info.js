31ad65bf7206f4c745fc936897611a109b36345d
Marcos <aurelio.marcos@ufv.br>
2023-07-06 09:13:22 -0300

    Classe info
    * Responsável por exibir e atualizar as inforamções sobre o desempenho
      em uma tabela

---
+++ IA-game/info.js
@@ -0,0 +1,25 @@
+class HelperInfo{
+  constructor(){
+    this.nGames=document.querySelector("#nGames");
+    this.score=document.querySelector("#score");
+    this.record=document.querySelector("#record");
+    this.meanScore=document.querySelector("#mean-score");
+  }
+
+  updateScore(newScore){
+    this.score.innerText=newScore;    
+    
+    if(newScore > this.record.innerText){
+      this.score.classList.add('red-color');
+    }else{
+      this.score.classList.remove('red-color')
+    }
+  }
+  updateNGamesAndRecord(nGames,record,meanScore){
+    this.nGames.innerText=nGames;
+    this.record.innerText=record;
+    this.meanScore.innerText=meanScore.toFixed(2);
+  }
+}
+
+export const Info = HelperInfo;
